7490a6158b74ab5012729293e7c62e8508fc3c0d
ㅁ <>
2022-06-25 12:50:58 +0900

    Java day1

---
+++ java_tutorial/src/day01/Syso2.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class Syso2 {
+	public static void main(String[] args) {
+		System.out.println("이름 : 한채영");
+		System.out.println("생일 : 2001년 1월 15일");
+		System.out.println("키 : 173cm");
+	}
+}
---
+++ java_tutorial/src/day01/Var3.java
@@ -0,0 +1,24 @@
+package day01;
+
+public class Var3 {
+	public static void main(String[] args) {
+		// 변수로 이름, 생년월일, 키를 저장해놓고 사용
+		String 이름 = "홍길동";
+		String 생년월일 = "2001년 1월 15일";
+		int 키 = 160;
+		
+		
+		System.out.println("이름 : "+이름);
+		System.out.println("생일 : "+생년월일);
+		System.out.println("키 : "+키+"cm");
+		/*
+		   이름 : 홍길동
+		   생일 : 2001년 1월 15일 
+		   키 : 175.5cm
+		   
+		   단, 이름과 생년월일, 키는 변수로 저장해서 사용
+		 */
+	}
+
+
+}
